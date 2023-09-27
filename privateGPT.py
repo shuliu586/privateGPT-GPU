4c0c7da4620704b47ef6cea3a3b35e9e731a78e8
shuliu586 <78126220+shuliu586@users.noreply.github.com>
2023-09-27 15:01:01 +0800

    Update privateGPT.py

--- privateGPT.py
+++ privateGPT.py
@@ -8,6 +8,8 @@ from langchain.llms import GPT4All, LlamaCpp
 import chromadb
 import os
 import argparse
+from utils import *
+from torch import cuda as torch_cuda
 import time
 
 if not load_dotenv():
@@ -20,15 +22,38 @@ persist_directory = os.environ.get('PERSIST_DIRECTORY')
 model_type = os.environ.get('MODEL_TYPE')
 model_path = os.environ.get('MODEL_PATH')
 model_n_ctx = os.environ.get('MODEL_N_CTX')
+is_gpu_enabled = (os.environ.get('IS_GPU_ENABLED', 'False').lower() == 'true')
 model_n_batch = int(os.environ.get('MODEL_N_BATCH',8))
 target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 
 from constants import CHROMA_SETTINGS
 
+def get_gpu_memory() -> int:
+    """
+    Returns the amount of free memory in MB for each GPU.
+    """
+    return int(torch_cuda.mem_get_info()[0]/(1024**2))
+
+def calculate_layer_count() -> int | None:
+    """
+    Calculates the number of layers that can be used on the GPU.
+    """
+    if not is_gpu_enabled:
+        return None
+    LAYER_SIZE_MB = 120.6 # This is the size of a single layer on VRAM, and is an approximation.
+    # The current set value is for 7B models. For other models, this value should be changed.
+    LAYERS_TO_REDUCE = 6 # About 700 MB is needed for the LLM to run, so we reduce the layer count by 6 to be safe.
+    if (get_gpu_memory()//LAYER_SIZE_MB) - LAYERS_TO_REDUCE > 32:
+        return 32
+    else:
+        return (get_gpu_memory()//LAYER_SIZE_MB-LAYERS_TO_REDUCE)
+
 def main():
+    ensure_integrity(persist_directory, False)
     # Parse the command line arguments
     args = parse_arguments()
-    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+    embeddings_kwargs = {'device': 'cuda'} if is_gpu_enabled else {}
+    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name, model_kwargs=embeddings_kwargs)
     chroma_client = chromadb.PersistentClient(settings=CHROMA_SETTINGS , path=persist_directory)
     db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS, client=chroma_client)
     retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
@@ -37,7 +62,7 @@ def main():
     # Prepare the LLM
     match model_type:
         case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+            llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False, n_gpu_layers=calculate_layer_count())
         case "GPT4All":
             llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
         case _default:
